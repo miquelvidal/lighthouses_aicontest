6ef792b1ead3c473da0795633bef7159b8793ee2
Miquel Vidal <miquel@miquelvidal.com>
2018-07-27 23:57:10 +0200

    Update mkmusbot.py

--- mkmusbot/mkmusbot.py
+++ mkmusbot/mkmusbot.py
@@ -4,22 +4,59 @@
 import random, sys
 import interface
 
-class RandBot(interface.Bot):
+class MkmusBot(interface.Bot):
     """Bot que juega aleatoriamente."""
-    NAME = "RandBot"
+    NAME = "MkmusBot"
 
     def play(self, state):
         """Jugar: llamado cada turno.
         Debe devolver una acción (jugada)."""
         cx, cy = state["position"]
+        #calcular el cuadrante
+        size_y = len(self.map)
+        size_x = len(self.map[0])
+        cota_x = size_x/2
+        cota_y = size_y/2
+	faros_por_cuadrante = [0,0,0,0]
+	mejor_cuadrante = 0
+
+        #calcular cuadrante con mas faros
+
+
         lighthouses = dict((tuple(lh["position"]), lh)
                             for lh in state["lighthouses"])
+              
+        #for dest in self.lighthouses:
+        for dest in state["lighthouses"]:
+            (faro_x, faro_y) = dest["position"]
+            if ((faro_x<=cota_x) and (faro_y<=cota_y)):
+                faros_por_cuadrante+=[1,0,0,0]
+            if ((faro_x>cota_x) and (faro_y<=cota_y)):
+                faros_por_cuadrante+=[0,1,0,0]
+            if ((faro_x<=cota_x) and (faro_y>cota_y)):
+                faros_por_cuadrante+=[0,0,1,0]
+            if ((faro_x>cota_x) and (faro_y>cota_y)):
+                faros_por_cuadrante+=[0,0,0,1]                 
+        for indice in [1,2,3]:
+             if (faros_por_cuadrante[mejor_cuadrante]<faros_por_cuadrante[indice]): 
+                     mejor_cuadrante=indice
+        posible_x = 0
+	posible_y = 0
+	cuadrante_bot = 0
+	if ((cx<=cota_x) and (cy<=cota_y)):
+		cuadrante_bot = 0
+	if ((faro_x>cota_x) and (faro_y<=cota_y)):
+		cuadrante_bot = 1
+	if ((faro_x<=cota_x) and (faro_y>cota_y)):
+		cuadrante_bot = 2
+	if ((faro_x>cota_x) and (faro_y>cota_y)):
+		cuadrante_bot = 3
 
-        # Si estamos en un faro...
+	# Si estamos en un faro...
         if (cx, cy) in self.lighthouses:
             # Probabilidad 60%: conectar con faro remoto válido
             if lighthouses[(cx, cy)]["owner"] == self.player_num:
-                if random.randrange(100) < 60:
+                if random.randrange(100) < 50:
                     possible_connections = []
                     for dest in self.lighthouses:
                         # No conectar con sigo mismo
@@ -37,7 +74,7 @@ class RandBot(interface.Bot):
                         return self.connect(random.choice(possible_connections))
 
             # Probabilidad 60%: recargar el faro
-            if random.randrange(100) < 60:
+            if random.randrange(100) < 55:
                 energy = random.randrange(state["energy"] + 1)
                 return self.attack(energy)
 
@@ -49,5 +86,5 @@ class RandBot(interface.Bot):
         return self.move(*move)
 
 if __name__ == "__main__":
-    iface = interface.Interface(RandBot)
+    iface = interface.Interface(MkmusBot)
     iface.run()
